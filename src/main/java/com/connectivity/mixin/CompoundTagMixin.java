cc485db4e1d5145d4a3f231dc458b7a546e32f12
someaddons <someaddons@github.com>
2023-01-13 15:51:28 +0100

    1.19.3

--- src/main/java/com/connectivity/mixin/CompoundTagMixin.java
+++ src/main/java/com/connectivity/mixin/CompoundTagMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(targets = "net/minecraft/nbt/CompoundTag$1")
 public class CompoundTagMixin
 {
-    @Redirect(method = "load(Ljava/io/DataInput;ILnet/minecraft/nbt/NbtAccounter;)Lnet/minecraft/nbt/CompoundTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtAccounter;accountBits(J)V"))
+    @Redirect(method = "load(Ljava/io/DataInput;ILnet/minecraft/nbt/NbtAccounter;)Lnet/minecraft/nbt/CompoundTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtAccounter;accountBytes(J)V"))
     public void on(final NbtAccounter instance, final long p_128923_)
     {
 
