34734a5ede577033ae4df6e6b53bab6af26fa76b
someaddons <someaddons@github.com>
2025-01-03 21:42:40 +0100

    6.5: Better logging for bytebuffers Logging Bundled packets now list their contained packets Add new feature which avoids an issue with chunksection sync sending less data than expected

--- src/main/java/com/connectivity/mixin/networkstats/CCustomPayloadLoginPacketNameMixin.java
+++ src/main/java/com/connectivity/mixin/networkstats/CCustomPayloadLoginPacketNameMixin.java
@@ -1,17 +1,26 @@
 package com.connectivity.mixin.networkstats;
 
 import com.connectivity.networkstats.INamedPacket;
+import net.minecraft.network.protocol.PacketType;
 import net.minecraft.network.protocol.login.ServerboundCustomQueryAnswerPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ServerboundCustomQueryAnswerPacket.class)
-public class CCustomPayloadLoginPacketNameMixin implements INamedPacket
+public abstract class CCustomPayloadLoginPacketNameMixin implements INamedPacket
 {
+    @Shadow
+    public abstract PacketType<ServerboundCustomQueryAnswerPacket> type();
+
     private transient String packetName = "";
 
     @Override
     public String getName()
     {
+        if (packetName.isEmpty())
+        {
+            return this.type().id().toString();
+        }
         return packetName;
     }
 
