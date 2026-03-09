e5e8758505e7389532f8ae933857cc98bde5cef6
someaddons <someaddons@github.com>
2026-03-09 15:41:52 +0100

    1.21.11

--- src/main/java/com/connectivity/mixin/ClientboundCustomQueryPacketMixin.java
+++ src/main/java/com/connectivity/mixin/ClientboundCustomQueryPacketMixin.java
@@ -5,7 +5,7 @@ import com.google.common.base.Charsets;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.login.ClientboundCustomQueryPacket;
 import net.minecraft.network.protocol.login.custom.DiscardedQueryPayload;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ClientboundCustomQueryPacketMixin
 {
     @Inject(method = "readUnknownPayload", at = @At(value = "INVOKE", target = "Ljava/lang/IllegalArgumentException;<init>(Ljava/lang/String;)V"), cancellable = true)
-    private static void onLargePacket(final ResourceLocation resourceLocation, final FriendlyByteBuf data, final CallbackInfoReturnable<DiscardedQueryPayload> cir)
+    private static void onLargePacket(final Identifier resourceLocation, final FriendlyByteBuf data, final CallbackInfoReturnable<DiscardedQueryPayload> cir)
     {
         if (Connectivity.config.getCommonConfig().debugPrintMessages)
         {
